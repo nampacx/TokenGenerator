04a0c8bb3e875885356c151fa3c54ab030bdc0f7
Bert Hoorne <behoorne@microsoft.com>
2020-03-23 16:27:40 +0100

    init

---
+++ DirectLinePayload.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TokenGenerator
+{
+    public class DirectLinePayload
+    {
+        public string conversationId { get; set; }
+        public string token { get; set; }
+        public int expires_in { get; set; }
+    }
+}
